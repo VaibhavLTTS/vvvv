40c2c21d9b865d2a5ab60d65f803eb2fc5a548ae
vaibhav singh <78896943+VaibhavLTTS@users.noreply.github.com>
2021-04-30 11:23:06 +0530

    Add files via upload

---
+++ Patients/src/main/java/com/ltts/service/PatientService.java
@@ -0,0 +1,36 @@
+package com.ltts.service;
+import java.util.List;
+
+import com.ltts.model.*;
+
+public class PatientService {
+	
+	
+	public Patient AddPatient(Patient patient) {
+		return null;
+	}
+
+	
+	
+	public Patient getPatientbyId(Integer P_id) {
+		return null;
+	}
+
+	public List <Patient> getPatientbyBP(String P_Blood_Pressure) {
+		return null;
+	}
+	
+	public List<Patient> getPatientbyOL (String P_Oxygen_Level) {
+		return null;
+	}
+	public List<Patient> getPatientbyHR(String P_Heart_Rate) {
+		return null;
+	}
+	
+	public List<Patient> getPatientbyName(String P_name) {
+		return null;
+	}
+	
+	
+}
+
